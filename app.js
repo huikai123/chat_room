3df4fe72d7f47fc259c33bfcb18a22dbeba67656
Kai <huikaizhong@yahoo.com>
2016-11-30 14:28:12 -0500

    basic div setup & on click listener

---
+++ app.js
@@ -0,0 +1,22 @@
+//require express module
+var express = require ('express'),
+	app = express(),
+//create a HTTP server
+	server = require ('http').createServer(app),
+	io = require('socket.io').listen(server);
+//listen to port 3000
+server.listen(3000);
+
+//create a route, set it root directory
+app.get('/', function (req,res){
+	res.sendFile(__dirname + '/public/index.html');
+});
+
+//receive message from browser
+io.sockets.on('connection', function(socket){
+	//same name as send message 
+	socket.on('send message', function(data){
+		//send message to all users include me
+		io.socket.emit('new message', data);
+	});
+});
