2a7587181fbcfb490b81e8ad883176db56c72fc1
Kai <huikaizhong@yahoo.com>
2016-11-30 17:24:28 -0500

    add usernames

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ var express = require ('express'),
 	app = express(),
 //create a HTTP server
 	server = require ('http').createServer(app),
-	io = require('socket.io').listen(server);
+	io = require('socket.io').listen(server),
+//create array to hold nicknames
+	nicknames = [];
 //listen to port 3000
 server.listen(3000);
 
@@ -12,11 +14,40 @@ app.get('/', function (req,res){
 	res.sendFile(__dirname + '/public/index.html');
 });
 
-//receive message from browser; load codes for socket
+//load codes for socket
 io.sockets.on('connection', function(socket){
-	//same name as send message 
+	//receive nicknames from client side
+	socket.on('new user', function(data,callback){
+		// check to see if nicknames exist or not
+		if (nicknames.indexOf(data)!= -1){
+			callback(false);
+		} else {
+			callback(true);
+			//store nickname to socket
+			socket.nickname = data;
+			//add nickname in the array
+			nicknames.push(socket.nickname);
+			updateNicknames();
+		}
+	});
+
+	//all users can see new nicknames
+	function updateNicknames(){
+		io.sockets.emit('usernames', nicknames);
+	}
+	
+	//receive message from browser
 	socket.on('send message', function(data){
-		//send message to all users include me
-		io.sockets.emit('new message', data);
+		//send message to all users include me, with username
+		io.sockets.emit('new message', {msg: data, nick: socket.nickname});
+	});
+
+	//disconnect from server
+	socket.on('disconnect', function(data){
+		//if no username entered, return
+		if(!socket.nickname) return;
+		//remove nickname from array
+		nicknames.splice(nicknames.indexOf(socket.nickname), 1);
+		updateNicknames();
 	});
 });
