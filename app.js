ccf42ff94c64a6d42a965f66940e219dce75cb12
Kai <huikaizhong@yahoo.com>
2016-12-06 12:44:43 -0500

    add loading history

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 //require express module
 var express = require ('express'),
 	app = express(),
+//require mongoose for mongodb
+	mongoose = require ('mongoose'),
 //create a HTTP server
 	server = require ('http').createServer(app),
 	io = require('socket.io').listen(server),
@@ -9,6 +11,25 @@ var express = require ('express'),
 //listen to port 3000
 server.listen(3000);
 
+//connect to mongodb
+mongoose.connect('mongodb://localhost/chat', function(err){
+	if (err){
+		console.log("There is problem with Mongo connection" + err);
+	} else {
+		console.log("connected to mongodb!");
+	}
+});
+
+//create a schema
+var chatSchema = mongoose.Schema({
+	nick: String,
+	msg: String,
+	created: {type: Date, default: Date.now}
+});
+
+//create a model
+var Chat = mongoose.model('Message', chatSchema);
+
 //create a route, set it root directory
 app.get('/', function (req,res){
 	res.sendFile(__dirname + '/public/index.html');
@@ -16,6 +37,12 @@ app.get('/', function (req,res){
 
 //load codes for socket
 io.sockets.on('connection', function(socket){
+	var query = Chat.find({}); 
+	query.sort('-created').limit(20).exec(function(err,docs){
+		if(err) throw err;
+		socket.emit('load old msg', docs);
+	});
+
 	//receive nicknames from client side
 	socket.on('new user', function(data,callback){
 		// check to see if users exist or not
@@ -40,31 +67,36 @@ io.sockets.on('connection', function(socket){
 	socket.on('send message', function(data, callback){
 		//trim spaces before and after
 		var msg = data.trim();
+		console.log('after trimming message is: ' + msg);
 		// use "/w " to begin private message
 		if (msg.substr(0,3) === "/w "){
-			console.log('pass 1st if');
 			msg = msg.substr(3);
-			var ind = msg.indexOf('');
+			//space after username
+			var ind = msg.indexOf(' ');
 			//if there is input in message box
 			if(ind !== -1){
 				//this part is user name
 				var name = msg.substring(0, ind);
 				//message starts here
 				var msg = msg.substring(ind + 1);
-				console.log('pass 2nd if');
 				//if user name is in the list
 				if (name in users){
-					users [name].emit('whisper', {msg:msg, nick: socket.nickname});
+					users [name].emit('whisper', {msg: msg, nick: socket.nickname});
+					console.log('message sent is: ' + msg);
 					console.log('Whisper!');
 				} else {
-					callback('Error! Enter a valid user.')
+					callback('Error! Enter a valid user.');
 				}			
 			} else{
-				callback('Error! Please enter a valid message for your whisper.')
+				callback('Error! Please enter a valid message for your whisper.');
 			}
 		} else{
+			var newMsg = new Chat({msg: msg, nick: socket.nickname});
+			newMsg.save(function(err){
+				if (err) throw err;
 			//send message to all users include me, with username
 			io.sockets.emit('new message', {msg: msg, nick: socket.nickname});
+			});
 		}
 	});
 
